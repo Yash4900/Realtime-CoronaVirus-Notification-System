fd48bc438297c488853fc00d60f3cb3639a4e32b
Yash4900 <satra.y@somaiya.edu>
2021-06-10 22:48:26 +0530

    updated

--- covid_notification.py
+++ covid_notification.py
@@ -10,50 +10,43 @@ def notify(title, message):
 	message = message,
 	app_icon = "img/icon.ico",#only .ico file will work
 	timeout = 10
-	)
-
+)
 
 #get data from webpage
 def getData(url):
 	r = requests.get(url)
 	return r.text
 
-
-
 if __name__ == "__main__":
 
 	while True:
 
-		htmlData = getData('https://www.mohfw.gov.in/')
+		htmlData = getData('https://www.deccanherald.com/national/coronavirus-india-karnataka-maharashtra-delhi-tamil-nadu-kerala-update-state-wise-total-number-of-confirmed-cases-vaccine-deaths-on-june-8-995035.html')
 
 		#represent html as nested data structure
 		soup = BeautifulSoup(htmlData, 'html.parser')
 
-		data = ""
+		data = []
 		#finding text inside all tr elements and appending into single string
 		for tr in soup.find_all('tbody')[0].find_all('tr'):
-			data += tr.get_text()
-	
+			temp = []
+			# Finding data in columns
+			for td in tr.find_all('td'):
+				temp.append(td.get_text())
+			data.append(temp)
+		
+		# Removing head of table
 		data = data[1:]
 
-		#separate state wise and making a list
-		dataList = data.split("\n\n")
 
-		#list of states for which you want to get notifications
-		states = ['Andhra Pradesh','Maharashtra','Gujarat']
-
-		for info in dataList[0:26]:
-			#separating data of a state(sr no, state, cases, cured, deaths)
-			stateInfo = info.split("\n")
-
-			#generate notiication for only those states which are mentioned in list
-			if stateInfo[1] in states:
-				#set title and message
-				notification_title = 'Updates of COVID-19'
-				notification_message = f"State : {stateInfo[1]}\nCases(Including foreign Nationals) : {stateInfo[2]}\nCured : {stateInfo[3]}\nDeaths : {stateInfo[4]}" 
-				#call notify function
-				notify(notification_title, notification_message)
-				time.sleep(3)
+		for state_info in data:
+			
+			#set title and message
+			notification_title = 'Update of COVID-19'
+			notification_message = f"State : {state_info[0]}\nCases : {state_info[1]}\nDeaths : {state_info[2]}" 
+			#call notify function
+			notify(notification_title, notification_message)
+			time.sleep(5)
 
 		#to get notifications in every half an hour
 		time.sleep(1800)
